1fbea3414168d505f98365a0f5e0ed22f010eca1
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-12-16 13:11:21 +0100

    finished create post, temporary redirect

--- app/posts/store.php
+++ app/posts/store.php
@@ -1,5 +1,7 @@
 <?php
 
+// add validate filename, characters and length
+
 declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
@@ -8,11 +10,9 @@ if (!isLoggedIn()) {
     redirect('/');
 }
 
-
 if (isset($_POST['description'], $_FILES['image'])) {
     $description = filter_var(trim($_POST['description']), FILTER_SANITIZE_STRING);
     $image = $_FILES['image'];
-    // $date = date(DATE_ATOM);
 
     if (count($_FILES) > 1) {
         $_SESSION['errors'] = "You can only upload 1 image in a post";
@@ -29,15 +29,30 @@ if (isset($_POST['description'], $_FILES['image'])) {
         redirect('/createpost.php');
     }
 
-    //fixa filename uuid, spara filnamnet i databasen
-    //databasen har automatisk date('now', 'localtime')
-    //lägg filen i uplods/posts eller nått
-    $fileName = date('Ymd');
-    move_uploaded_file($image['tmp_name'], '../../uploads/' . $fileName . ".jpg");
+    $fileExt = '.' . explode('/', $image['type'])[1];
+    $fileName = uniqid("", true) . $fileExt;
+    $id = $_SESSION['user']['id'];
+
+    if (!move_uploaded_file($image['tmp_name'], '../../uploads/posts/' . $fileName)) {
+        $_SESSION['errors'] = "Something went wrong with the upload";
+        redirect('/createpost.php');
+    }
+
+    $statement = $pdo->prepare('INSERT INTO posts (user_id, image, description) VALUES (:user_id, :image, :description)');
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $statement->execute([
+        ':user_id' => $id,
+        ':image' => $fileName,
+        ':description' => $description
+    ]);
+
+    //temporary redirect
+    $_SESSION['messages'] = "post uploaded!";
+    redirect('/createpost.php');
 }
 
-
-
-// array(5) { ["name"]=> string(36) "146a4f991a319d73a717baeb54270d5a.jpg" ["type"]=> string(10) "image/jpeg" ["tmp_name"]=> string(66) "/private/var/folders/b7/jzmb2fls2ysdnr8v6l4z1ghr0000gn/T/phpezo7PW" ["error"]=> int(0) ["size"]=> int(282194) }
-
-// redirect('/');
+redirect('/');
