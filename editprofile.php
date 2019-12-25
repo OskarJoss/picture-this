9b299465a58cbd5993cb0eea068a11a02a775fc5
oskarjoss <oskar.sjolin.joss@gmail.com>
2019-12-25 17:39:39 +0100

    more on edit profile

--- editprofile.php
+++ editprofile.php
@@ -28,7 +28,16 @@
     <div class="avatar-container">
         <img class="avatar" src="/uploads/avatars/<?php echo $user['avatar'] ?>" alt="avatar">
     </div>
-</section>
+    <button class="show-form-button">Change Avatar</button>
 
+    <form class="edit-profile-avatar" action="app/users/editprofile.php" method="post" enctype="multipart/form-data">
+        <div class="form-section">
+            <input type="file" name="image" id="image" accept=".jpg, .jpeg, .png" required>
+            <button type="submit">Save</button>
+        </div>
+    </form>
+
+
+</section>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,7 @@
 </div><!-- /container -->
 
 <script src="/assets/scripts/likes.js"></script>
+<script src="/assets/scripts/editprofile.js"></script>
 </body>
 
 </html>
